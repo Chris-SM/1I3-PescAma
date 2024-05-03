3b8356f478f9fd43ad18c4a57540739e0c5146a1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:18:45 -0300

    Metade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿bool lago;
+string txt;
+double kgPeixe,multa = 0;
+Console.WriteLine("Avaliação de Legalidade de pesca");
+Console.Write(@"Onde Pescou?
+C - Águas continentais e estuarinas
+M - Águas marinhas
+");
+txt = Console.ReadLine()!;
+if(txt == "M"){lago = true;}
+else if(txt == "C"){lago = false;}
+else{ return;}
+Console.Write("");
